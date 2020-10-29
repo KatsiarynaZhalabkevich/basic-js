f836340d184ccfb05dd77f10cc9b0607ceb4be5e
Katsiaryna Zhalabkevich <beluzhka@gmail.com>
2020-10-30 01:04:07 +0300

    feat:cats and dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
+//++++
+module.exports = function createDreamTeam(members) {
+    if(!Array.isArray(members)){
+        return false;
+    }
+    let team = '';
+    let result = '';
+    for (let i = 0; i < members.length; i++) {
+        if( typeof members[i] === "string" ){
+          members[i]= members[i].trim();
+            team += members[i].charAt(0).toUpperCase();
+        }
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    }
+    team = team.split('');
+   team = team.sort();
+    console.log(team);
+    for (let i = 0; i < team.length; i++) {
+        result += team[i];
+    }
+    return result;
 };
