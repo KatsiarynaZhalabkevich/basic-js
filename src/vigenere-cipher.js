4357bf414351fae7c06e65148a8c980f30c4def4
Katsiaryna Zhalabkevich <beluzhka@gmail.com>
2020-10-31 15:34:29 +0300

    feat: vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,67 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(isDirect = true) {
+        this.isDirect = isDirect;
+    }
+
+//большие буквы 65-90
+    encrypt(message, key) {
+        this.validate(message, key);
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let result = '';
+        for (let i = 0, j = 0; i < message.length; i++, j++) {
+            if (message.charAt(i).match(/[A-Z]/)) { //только латинский алфавит
+                if (j === key.length) {
+                    j = 0;
+                }
+                let newCharCode = message.charCodeAt(i) + key.charCodeAt(j) - 65; // из маленькой сделать большу-65
+                while (newCharCode > 90) {
+                    newCharCode = newCharCode - 26;
+                }
+                result += String.fromCharCode(newCharCode);
+            } else {
+                result += message.charAt(i);
+                j--;
+            }
+
+        }
+        return !this.isDirect ? this.reverse(result) : result;
+    }
+
+    decrypt(message, key) {
+        this.validate(message, key);
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let result = '';
+        for (let i = 0, j = 0; i < message.length; i++, j++) {
+            if (message.charAt(i).match(/[A-Z]/)) { //зачем проверять? по коду все равно что-то найдется
+                if (j === key.length) {
+                    j = 0;
+                }
+                let newCharCode = message.charCodeAt(i) - key.charCodeAt(j) + 26 + 65; // из маленькой сделать большу +26+65
+                while (newCharCode > 90) {
+                    newCharCode = newCharCode - 26;
+                }
+                result += String.fromCharCode(newCharCode);
+            } else {
+                result += message.charAt(i);
+                j--;
+            }
+
+        }
+        return !this.isDirect ? this.reverse(result) : result;
+    }
+
+
+    reverse(result) {
+        return result.split('').reverse().join('');
+    }
+
+    validate(message, key) {
+        if (message === null || key === null || message === undefined || key === undefined) {
+            throw new Error('Not valid arguments');
+        }
+    }
 }
 
 module.exports = VigenereCipheringMachine;
